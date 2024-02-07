e0abd3090ec554575312c346af95f2cc3961ca97
ShlomoBluth <lomibluth@gmail.com>
2024-02-07 13:21:59 +0200

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
   videoe:true,
   projectId: 'x3jgv2',
   screenshotOnRunFailure: false,
-  defaultCommandTimeout: 30000*10,
+  defaultCommandTimeout: 30000,
   requestTimeout: 30000,
   pageLoadTimeout: 300000,
   experimentalFetchPolyfill: true,
